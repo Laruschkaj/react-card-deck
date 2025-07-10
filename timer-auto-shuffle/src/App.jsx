9e65ed8c96fffd12e7916df714fee73ab634e186
Laruschkaj <laruschkajoubert@gmail.com>
2025-07-10 19:42:42 -0400

    Updated code for both 'cards' and 'timer-auto-shuffle' projects

--- timer-auto-shuffle/src/App.jsx
+++ timer-auto-shuffle/src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Deck from "./Deck.jsx"
 
 function App() {
-    return (
-        <div>
-            <Deck />
-        </div>
-    );
+  return (
+    <div>
+      <Deck />
+    </div>
+  );
 }
 
 export default App;
